4a4175f9942e87bdd54e7eae50958ebb7dcbfbac
CaipirUltron <matheus.ferreira.reis@gmail.com>
2020-12-19 20:42:28 +0000

    Rviz visualization working with robot pose and map. Obstacles are still missing.

--- src/2D_World_with_obstacles/src/rviz_simulation.py
+++ src/2D_World_with_obstacles/src/rviz_simulation.py
@@ -6,7 +6,7 @@ import tf
 
 from rospy.numpy_msg import numpy_msg
 from visualization_msgs.msg import Marker, MarkerArray
-from geometry_msgs.msg import Point, Pose, PoseStamped
+from geometry_msgs.msg import Point32, Pose, PoseStamped, PolygonStamped
 from nav_msgs.msg import OccupancyGrid
 from tf.transformations import quaternion_from_euler
 
@@ -23,51 +23,71 @@ class GridWorldSimulation():
 
         # ROS Subscribers
         # click_sub = rospy.Subscriber("clicked_point", PointStamped, update_reference)
-        self.robot_sub = rospy.Subscriber("robot_state", Pose, self.update_robot)
+        self.robot_sub = rospy.Subscriber("robot_state", Pose, self.draw_robot)
 
         # ROS Publishers
+        self.world_walls_publisher = rospy.Publisher('world_walls', PolygonStamped, queue_size=1)
         self.robot_marker_publisher = rospy.Publisher('robot_marker', Marker, queue_size=1)
         self.angle_marker_publisher = rospy.Publisher('angle_marker', PoseStamped, queue_size=1)
 
         # Setup tf tree and rviz objects
         self.world_tf = tf.TransformBroadcaster()
-        self.robot_position_marker = Marker()
-        self.robot_angle_marker = PoseStamped()
+        self.world_walls = PolygonStamped()
+        self.robot_position = Marker()
+        self.robot_angle = PoseStamped()
+    
+        # Initialize world polygon
+        self.world_walls.header.frame_id = "map_frame"
+        self.world_walls.polygon.points = world._corners
+        for i in range(len(self.world_walls.polygon.points)):
+            self.world_walls.polygon.points[i].z = 0
+
+        # Initialize world obstacles
+        
 
         # Initialize robot position marker
-        self.robot_position_marker.header.frame_id = "robot_frame"
-        self.robot_position_marker.type = self.robot_position_marker.CYLINDER
-        self.robot_position_marker.action = self.robot_position_marker.ADD
-        self.robot_position_marker.scale.x = 1.0
-        self.robot_position_marker.scale.y = 1.0
-        self.robot_position_marker.scale.z = 0.1
-        self.robot_position_marker.color.a = 1.0
-        self.robot_position_marker.color.r = 0.0
-        self.robot_position_marker.color.g = 0.0
-        self.robot_position_marker.color.b = 1.0
-        self.robot_position_marker.pose.position.x = 0.0
-        self.robot_position_marker.pose.position.y = 0.0
-        self.robot_position_marker.pose.position.z = -0.1
-        self.robot_position_marker.pose.orientation.x = 0.0
-        self.robot_position_marker.pose.orientation.y = 0.0
-        self.robot_position_marker.pose.orientation.z = 0.0
-        self.robot_position_marker.pose.orientation.w = 1.0
+        self.robot_position.header.frame_id = "robot_frame"
+        self.robot_position.type = self.robot_position.CYLINDER
+        self.robot_position.action = self.robot_position.ADD
+        self.robot_position.scale.x = 1.0
+        self.robot_position.scale.y = 1.0
+        self.robot_position.scale.z = 0.1
+        self.robot_position.color.a = 1.0
+        self.robot_position.color.r = 0.0
+        self.robot_position.color.g = 0.0
+        self.robot_position.color.b = 1.0
+        self.robot_position.pose.position.x = 0.0
+        self.robot_position.pose.position.y = 0.0
+        self.robot_position.pose.position.z = -0.1
+        self.robot_position.pose.orientation.x = 0.0
+        self.robot_position.pose.orientation.y = 0.0
+        self.robot_position.pose.orientation.z = 0.0
+        self.robot_position.pose.orientation.w = 1.0
 
         # Initialize robot orientation marker
-        self.robot_angle_marker.header.frame_id = "robot_frame"
-        self.robot_angle_marker.pose.position.x = 0.0
-        self.robot_angle_marker.pose.position.y = 0.0
-        self.robot_angle_marker.pose.position.z = 0.0
-        self.robot_position_marker.pose.orientation.x = 0.0
-        self.robot_position_marker.pose.orientation.y = 0.0
-        self.robot_position_marker.pose.orientation.z = 0.0
-        self.robot_position_marker.pose.orientation.w = 1.0
+        self.robot_angle.header.frame_id = "robot_frame"
+        self.robot_angle.pose.position.x = 0.0
+        self.robot_angle.pose.position.y = 0.0
+        self.robot_angle.pose.position.z = 0.0
+        self.robot_angle.pose.orientation.x = 0.0
+        self.robot_angle.pose.orientation.y = 0.0
+        self.robot_angle.pose.orientation.z = 0.0
+        self.robot_angle.pose.orientation.w = 1.0
 
-        # Initial map update
-        self.update_robot()
+        # Initial map and robot update
+        self.draw_world()
+        self.draw_robot()
 
-    # This function receives a Pose ROS msg and updates the corresponding transformation
-    def update_robot(self):
+    # This function draws the world and obstacles in rviz
+    def draw_world(self):
+
+        world_quat = quaternion_from_euler(0, 0, 0)
+        # Sends transformations and publishes Rviz graphical objects
+        self.world_tf.sendTransform((0,0,0),world_quat,rospy.Time.now(),"map_frame", "world")
+        self.world_walls_publisher.publish(self.world_walls)
+
+    # This function draws the robot in rviz
+    def draw_robot(self):
 
         robot_marker_position_x = self.world.robot.x
         robot_marker_position_y = self.world.robot.y
@@ -75,26 +95,26 @@ class GridWorldSimulation():
 
         # Sends transformations and publishes Rviz graphical objects
         self.world_tf.sendTransform((robot_marker_position_x,robot_marker_position_y,0),robot_quat,rospy.Time.now(),"robot_frame", "world")
-        self.robot_marker_publisher.publish(self.robot_position_marker)
-        self.angle_marker_publisher.publish(self.robot_angle_marker)
+        self.robot_marker_publisher.publish(self.robot_position)
+        self.angle_marker_publisher.publish(self.robot_angle)
 
 if __name__ == '__main__':
     try:
         # Initialize world
         sim_dt = 0.01
         sim_freq = 1/sim_dt
-        world = World(dt = sim_dt, width = 4, height = 4)
+        world = World(dt = sim_dt, width = 8, height = 8)
 
         # Add obstacles
-        world.add_obstacle(1.0, 1.0)
-        world.add_obstacle(1.0, 0.0)
-        world.add_obstacle(1.0, -1.0)
-        world.add_obstacle(0.0, 1.0)
-        world.add_obstacle(0.0, 0.0)
-        world.add_obstacle(0.0, -1.0)
-        world.add_obstacle(-1.0, 1.0)
-        world.add_obstacle(-1.0, 0.0)
-        world.add_obstacle(-1.0, -1.0)
+        # world.add_obstacle(1.0, 1.0)
+        # world.add_obstacle(1.0, 0.0)
+        # world.add_obstacle(1.0, -1.0)
+        # world.add_obstacle(0.0, 1.0)
+        # world.add_obstacle(0.0, 0.0)
+        # world.add_obstacle(0.0, -1.0)
+        # world.add_obstacle(-1.0, 1.0)
+        # world.add_obstacle(-1.0, 0.0)
+        # world.add_obstacle(-1.0, -1.0)
 
         # Give world to the agent
         agent = Agent(world = world)
@@ -109,12 +129,12 @@ if __name__ == '__main__':
         # Main ROS loop
         rate = rospy.Rate(sim_freq)  # 10hz
         while not rospy.is_shutdown():
-            # v = 2*(np.random.rand() - 0.5)
             v = 1
             w = 0
             agent.act(v, w)
 
-            turtleSimulation.update_robot()
+            turtleSimulation.draw_world()
+            turtleSimulation.draw_robot()
             print(world.robot)
 
             rate.sleep()
